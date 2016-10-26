8faa68a788638fb20cf860346d7c2e629fbd4702
emcd123 <mcdonnell.owen1@gmail.com>
2016-10-26 16:52:33 +0100

    minor text fixes

--- SequenceAlignmentAffine.pl
+++ SequenceAlignmentAffine.pl
@@ -2,24 +2,40 @@ use strict;
 use warnings;
 
 use Data::Dumper qw(Dumper);
+use List::Util qw(min max);
 
 my @seq1 = ('A','G','C','T','A','T');
 my @seq2 = ('A','A','G','T');
-my @Inner = (0,0,0);
 
-#@matrix = (\@seq1);
+#Create a multidimensional array(3d in this case) of specified size
+my ($x, $y, $z) = (6, 4, 3);
+my @matrix = map [map [map 0, 1..$z], 1..$y], 1..$x;
 
-my @matrix = ();
-#my $matrixRef = \@matrix;
+print Dumper \@matrix;
 
 
-foreach my $i (@seq1) {
-  foreach my $j (@seq2) {
-    push  @{$matrix[$i]} , $j;
-    foreach my $z (@inner){
-      push @{$matrix[$i][$j]} , $z;
-    }
+my $d = 3; #gap opening penalty
+my $e = 1; #gap extension penalty
+my $sTransversion = -2 ; # when Non complementary bases mismatch
+my $sTransition = -1; # when complementary bases mismatch
+my $sMatch = 1; #When bases match
+
+#Add in the operation rules
+my $IxRule = 0;
+my $IyRule = 0;
+my $MRule = 0;
+
+foreach $i (1..scalar @seq1){
+  foreach $j(1..scalar @seq2){
+    #evaluating the rules
+    my @Ix = ();
+    my @Iy = ();
+    my @M = ();
+
+    #find the maxes of those categorys, then push them to @matrix
+    my $maxIx = max @Ix;#etc.
+    my $maxIy = max @Iy;#etc.
+    my $Match = max @M;#etc.
+
   }
 }
-
-print Dumper \@matrix ;
